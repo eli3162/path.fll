510c117ff2c19fce18ea0ee32123e0a9372926d9
Ethan Li <fourdimentional31@gmail.com>
2026-02-07 12:57:45 -0500

    Added better path parsing

--- parse.py
+++ parse.py
@@ -1,10 +1,10 @@
 import os
 import sys
 from pathlib import Path
+import textwrap
 
 marker = "#path.fll-DATA"
 
-# Read the file (optionally pass input path as first arg)
 input_path = Path(sys.argv[1]) if len(sys.argv) > 1 else Path("file.txt")
 data = input_path.read_text(encoding="utf-8")
 
@@ -12,7 +12,15 @@ idx = data.find(marker)
 if idx != -1:
     data = data[:idx].rstrip("\r\n")
 
-data = '{"path": [' + data + ']}'
+data = data.replace("#PATH-POINTS-START Path", "")
+
+data = textwrap.indent(data, "    ")
+
+data = '[' + data + '\n]'
+
+data = textwrap.indent(data, "    ")
+
+data = '{"path": ' + '\n' + data + '\n}'
 
 output_path = Path(sys.argv[2]) if len(sys.argv) > 2 else Path("path.json")
 output_path.write_text(data, encoding="utf-8")
